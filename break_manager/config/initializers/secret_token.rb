84b73e554ff9a027fba1bef69e59a8ac897ec9d3
Amy Beaver <amyjbeaver@gmail.com>
2013-11-12 08:22:41 -0500

    initial creation

---
+++ break_manager/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BreakManager::Application.config.secret_key_base = '14930d92a8499b62241349d878f8ba6a6bba870b8ecbb6b947b805b0632d6a8353b1fdf1bc024108b3949e96185e27a46ded699e81d10609faf2ecfbb3839f31'
---
+++ thundercats/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Thundercats::Application.config.secret_key_base = 'a6be58b58c263d86554540116bf386af956121cc917024b121928be476adba97480260c5deace478ae974383a1ae57c5ca614af438be72c86e06a35c5b011e49'
